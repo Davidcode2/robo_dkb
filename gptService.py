744b74fb9209ebad495db3374e3c8c3d05529c22
Jakob Lingel <jlingel68@gmail.com>
2024-12-08 15:20:44 +0100

    Refine prompt

--- gptService.py
+++ gptService.py
@@ -24,7 +24,8 @@ class GptService:
                     "role": "system",
                     "content": (
                         "You are an assistant that categorizes financial transactions into JSON data "
-                        "based on user-provided categories. You output only JSON data."
+                        "based on user-provided categories. Ensure that each transaction is only assigned to one category."
+                        "You output only JSON data."
                         "Your output does not contain any additional text, explanations, or formatting."
                         "Ensure the output is valid, well-formed JSON, without any escape characters, extra spaces, or newlines."
                     ),
@@ -35,6 +36,11 @@ class GptService:
                         "Categorize the following transactions into the provided categories."
                         "Use the transaction amounts and assign them to the appropriate category array."
                         "Context: The transactions take place in germany. Use this information to accurately categorize the transactions."
+                        "Transactions to the Deutsche Bahn (DB) and to gas stations are to be categorized as travel."
+                        f"Transactions to {os.getenv("ACCOUNT_OWNER")} are to be categorized as investments"  
+                        f"Transactions to {os.getenv("HOUSEHOLD_MEMBER")} where the transaction details do not suggest otherwise are to be categorized as groceries"
+                        "Transactions to grocery stores like Aldi, Rewe, Kaufland, Edeka and so on, as well as to bakeries are to be categorized as groceries."
+                        "Transactions to stores that sell clothing, electronics, furniture, etc., and non-food in general are to be categorized as shopping."
                         "Return the result as JSON in the same structure as the input categories."
                         "Ensure only the numeric amounts are included in the arrays."
                         f"Categories: {categories}"
